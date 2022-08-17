2b381f099c5e014a979d55cb81cac5af2e027677
Daiane Reina <daiane.veras.reina@gmail.com>
2022-08-17 11:23:36 +0100

    add morgan

--- config/index.js
+++ config/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
+const logger = require("morgan");
+
 function makesConfig(app) {
   app.set("view engine", "hbs");
   app.use(express.urlencoded({ extended: false }));
+  app.use(logger("dev"));
 }
 
 module.exports = makesConfig;
